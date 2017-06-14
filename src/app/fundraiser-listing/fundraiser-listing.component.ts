f16a871d1b56ab0e287a88b60b297f29a74277e8
shahnirav <shah.nirav@me.com>
2017-06-14 21:36:54 +0100

    commit

--- src/app/fundraiser-listing/fundraiser-listing.component.ts
+++ src/app/fundraiser-listing/fundraiser-listing.component.ts
@@ -8,7 +8,7 @@ import {PostsService} from '../posts.service';
 })
 export class FundraiseListingComponent implements OnInit {
 
-  posts: any = [];
+  posts: any;
 
   constructor(private postService: PostsService, private zone: NgZone) {
   }
