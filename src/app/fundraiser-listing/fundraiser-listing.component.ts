243f454ac87780601113f8f31baa772ac2700fad
shahnirav <shah.nirav@me.com>
2017-06-14 21:45:40 +0100

    commit

--- src/app/fundraiser-listing/fundraiser-listing.component.ts
+++ src/app/fundraiser-listing/fundraiser-listing.component.ts
@@ -1,4 +1,4 @@
-import {Component, NgZone, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
 import {PostsService} from '../posts.service';
 
 @Component({
@@ -10,16 +10,16 @@ export class FundraiseListingComponent implements OnInit {
 
   posts: any;
 
-  constructor(private postService: PostsService, private zone: NgZone) {
+  constructor(private postService: PostsService, private cdRef: ChangeDetectorRef) {
   }
 
   ngOnInit() {
     this.postService.getAllPosts().subscribe(posts => {
-      this.zone.run(() => {
         this.posts = posts;
         console.log(this.posts.length);
+        this.cdRef.detectChanges();
       });
-    });
+
   };
 
 }
