10bb0948b6d4f06fc700eaab71dc07a8d0e21373
shahnirav <shah.nirav@me.com>
2017-07-09 21:11:07 +0100

    commit

--- server/routes/phone.js
+++ server/routes/phone.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const client = require('twilio')(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 module.exports = function (passport) {
 
   router.post('/api/phone/code/request', (req, res) => {
