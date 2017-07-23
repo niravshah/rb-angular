0afb65e42746e47c35692467c90d17220c3050a3
shahnirav <shah.nirav@me.com>
2017-07-23 13:26:06 +0100

    commit

--- src/app/index/home-header/header.component.ts
+++ src/app/index/home-header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../../login/login.service';
 
 @Component({
   selector: 'app-home-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: LoginService) { }
 
   ngOnInit() {
+  }
+
+  isLogin() {
+    return this.authService.loggedIn();
   }
 
 }
