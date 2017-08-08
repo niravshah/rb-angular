cfc87487a444de4644f2548c4b0e10e78d8c9e9a
Nirav <nirav.shah83@gmail.com>
2017-08-08 16:58:26 +0000

    commit

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(require('prerender-node').set('prerenderToken', 'xdmx04EGAKV1O7qvFNCy').set('afterRender', function(err, req, prerender_res) {
-	console.log('After Render',prerender_res)
-}));
+app.use(require('prerender-node').set('prerenderToken', 'xdmx04EGAKV1O7qvFNCy'));
 
 // Parsers for POST data
 app.use(bodyParser.json());
